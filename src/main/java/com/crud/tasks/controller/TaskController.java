2ef185c9a2901580c1d3147e50512b96df14b767
Kamil Biernat <k.biernat@yahoo.ca>
2022-05-23 13:25:26 +0200

    14.4 Creating Controller (@RequestMapping)

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,6 +1,5 @@
 package com.crud.tasks.controller;
 
-import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,14 +9,15 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/tasks")
 public class TaskController {
-    @GetMapping //@RequestMapping(method = RequestMethod.GET, value = "")
+
+    @GetMapping
     public List<TaskDto> getTasks() {
         return new ArrayList<>();
     }
 
-    @GetMapping
-    public TaskDto getTask(Long taskId) {
-        return new TaskDto(1L, "a", "a");
+    @GetMapping(value = "{taskId}")
+    public TaskDto getTask(@PathVariable Long taskId)  {
+        return new TaskDto(1L, "Test title", "test_content");
     }
 
     @DeleteMapping
@@ -25,13 +25,13 @@ public class TaskController {
 
     }
 
-    @PostMapping
+    @PutMapping
     public TaskDto updateTask (TaskDto taskDto) {
-        return new TaskDto(1L, "b", "b");
+        return new TaskDto(1L, "Edited test title", "Test content");
     }
 
-    @PutMapping
-    public void createTask (TaskDto task) {
+    @PostMapping
+    public void createTask (TaskDto taskDto) {
 
     }
 }
