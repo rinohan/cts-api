be80d1131e97903cfab47c1fb29ed3749040b5cf
Kamil Biernat <k.biernat@yahoo.ca>
2022-06-01 15:34:58 +0200

    17.1 WAR file created

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/tasks")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class TaskController {
 
     public final DbService service;
--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -1,11 +1,8 @@
 package com.crud.tasks.repository;
 
 import com.crud.tasks.domain.Task;
-import com.sun.xml.bind.v2.model.core.ID;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
-import javax.persistence.Id;
 import java.util.List;
 import java.util.Optional;
 
